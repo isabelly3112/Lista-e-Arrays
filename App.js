1fa3ba51944039be2a9c43cf619200a6ee86032c
ISABELLY RIBEIRO DOS SANTOS <isabelly.santos22@portalsesisp.org.br>
2026-05-26 12:02:06 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -21,10 +21,10 @@ const styles = StyleSheet.create({
 
 //--------------------------------------------------------------------------------------------------------------
 
-import {useState} from 'react'
+import { useState } from 'react'
 
 import {
-  View, 
+  View,
   Text,
   TextInput,
   Pressable,
@@ -40,7 +40,7 @@ export default function App() {
   const [tarefas, setTarefas] = useState([]) // Para a lista de tarefas 
 
   function adicionarTarefa() {
-    
+
     //Verifica se o campo da tarefa está vazio
     if (tarefa === '') {
       return
@@ -59,6 +59,161 @@ export default function App() {
   }
 
   function concluirTarefa(id) {
+    //Criar uma nova lista de tarefas atualizada, onde a tarefa com o id corresponde tem seu estado de conclusão invertido
+    const listaAtualizada = tarefas.map(item => {
 
+      if (item.id === id) {
+        return {
+          ...item,
+          concluida: !item.concluida
+        }
+      }
+      return item
+    })
   }
+
+  function excluirTarefa(id) {
+    //Criar uma nova lista de tarefas atualizada, onde a tarefa com o id 
+  }
+
+  // Renderizar a interface do aplicativo 
+  return (
+
+    <View style={style.container}>
+
+      <Text style={style.titulo}>
+        Gerenciador de Tarefas
+      </Text>
+
+      <Text style={style.label}>
+        Digite uma tarefa que deseja adicionar a lista.
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Digite uma tarefa'
+        value={tarefa}
+        onChangeText={setTarefa}
+      />
+
+      {/* Botão para adicionar tarefa */}
+      <Pressable
+        style={style.botao}
+        onPress={adicionarTarefa}
+      >
+        <Text style={styles.textoBotao}>
+          Adicionar
+        </Text>
+      </Pressable>
+
+      <Text>
+        Total de Tarefas: {tarefas.length}
+      </Text>
+
+      <FlatList
+        data={tarefas}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+
+          <View style={styles.item}>
+
+            <Pressable
+              onPress={() => concluirTarefa(item.id)}
+            >
+              <Text
+                style={[
+                  styles.nome,
+                  item.concluida && styles.concluida
+                ]}
+              >
+                {item.nome}
+              </Text>
+            </Pressable>
+
+            <Pressable
+              onPress={() => excluirTarefa(item.id)}
+            >
+              <Text style={style.excluir}>
+                X
+              </Text>
+            </Pressable>
+
+          </View>
+
+        )}
+      />
+
+    </View>
+
+  )
 }
+
+const style = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    marginTop: 50,
+    backgroundColor: '#F5F5F5'
+  },
+
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10
+  },
+
+  botao: {
+    backgroundColor: '#0dd57e',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginBottom: 20
+  },
+
+  textoBotao: {
+    color: '#ffffff',
+    fontWeight: 'bold'
+  },
+
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 10,
+    marginBottom: 10
+  },
+
+  nome: {
+    fontSize: 18
+  },
+
+  concluida: {
+    textDecorationLine: 'line-through',
+    color: '#999'
+  },
+
+  excluir: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+
+  label: {
+    fontSize: 18,
+    marginBottom: 10
+  }
+
+})
